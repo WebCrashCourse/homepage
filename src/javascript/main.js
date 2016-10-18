6596f57d66b5e5f5c65cea86e12ae78200832e55
Ilja <iljapanic@gmail.com>
2016-10-18 10:04:47 +0200

    add ios safari vh buggyfill, switch to local video sources

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
+  $('.hero').removeClass('is-loading')
+
   $('.hero').vide({
-    mp4: 'http://res.cloudinary.com/web-crash-course/video/upload/ac_none,q_100/v1476641122/For_Wes_dbxgqy.mp4',
-    webm: 'http://res.cloudinary.com/web-crash-course/video/upload/ac_none,q_100/v1476641122/For_Wes_dbxgqy.webm',
-    ogv: 'http://res.cloudinary.com/web-crash-course/video/upload/ac_none,q_100/v1476641122/For_Wes_dbxgqy.ogv',
-    poster: 'assets/images/For_Wes.jpg'
+    mp4: 'assets/video/For_Wes.mp4',
+    webm: 'assets/video/For_Wes.webm',
+    ogv: 'assets/video/For_Wes.ogv',
+    poster: 'assets/images/hero.jpg'
   }, {
     volume: 0,
     playbackRate: 0.65,
